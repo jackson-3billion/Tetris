414c3c5fe47d282dd6da6e9d2b3b5214781b892e
jc <jotace422@gmail.com>
2021-11-23 13:27:28 +0900

    feat: useSocket 커스텀 훅 작성

---
+++ client/hooks/useSocket.js
@@ -0,0 +1,26 @@
+import io from 'socket.io-client';
+import { useCallback, useEffect, useRef } from 'react';
+
+const useSocket = (server) => {
+  const socketRef = useRef();
+
+  const createSocket = useCallback(() => {
+    if (!socketRef?.current) {
+      socketRef.current = io(server);
+    }
+
+    return socketRef.current;
+  }, [server]);
+
+  useEffect(() => {
+    createSocket();
+
+    return () => {
+      createSocket().disconnect();
+    };
+  }, [createSocket]);
+
+  return socketRef;
+};
+
+export default useSocket;
