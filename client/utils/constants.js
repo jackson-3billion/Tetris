a23f13e755e359ccd6a00923a0f624c949bcd8b1
jc <jotace422@gmail.com>
2021-11-29 22:50:02 +0900

    feat: 게임 구현에 사용되는 여러 상수들을 별도의 파일로 분리

---
+++ client/utils/constants.js
@@ -0,0 +1,8 @@
+export const ARENA_WIDTH = 12;
+export const ARENA_HEIGHT = 20;
+export const PLAYER_INITIAL_POS = { x: ARENA_WIDTH / 2 - 2, y: 0 };
+export const DROP_SLOW = 1000;
+export const DROP_FAST = 70;
+export const LEFTWARD = { x: -1, y: 0 };
+export const RIGHTWARD = { x: 1, y: 0 };
+export const DOWNWARD = { x: 0, y: 1 };
