1ef78665ef282da6864da9f9772f721c3370906e
jc <jotace422@gmail.com>
2021-12-04 23:46:07 +0900

    [client] fix: 전달받은 initialValue를 사용해 state의 초기값으로 사용하도록 수정

--- client/hooks/useInput.js
+++ client/hooks/useInput.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 
-const useInput = () => {
-  const [value, setValue] = useState('');
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
   const handler = useCallback(({ target }) => setValue(target.value), []);
 
   return [value, setValue, handler];
