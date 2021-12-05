dfb359a96d8e76c0d02fc08950028e6c41e6a11a
jc <jotace422@gmail.com>
2021-12-05 23:20:17 +0900

    [Client] feat: 방 생성 페이지에서 입력값에 대한 유효성 검사 및 고유한 방 아이디 생성 로직 추가

--- client/pages/CreateRoom.js
+++ client/pages/CreateRoom.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router';
 import styled from '@emotion/styled';
+import { v4 as uuid } from 'uuid';
 
 import useInput from '@hooks/useInput';
+import useValidate from '@hooks/useValidate';
 
 const CreateRoom = () => {
-  const [nickname, setNickname, handler] = useInput();
+  const [nickname, handleInputChange] = useInput('');
+  const [isValid, msg] = useValidate(nickname);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('form submiitted');
-    console.log(nickname);
+    if (isValid) {
+      navigate(`/game/${uuid()}`, { state: nickname.trim() });
+    }
+  };
+
+  const handleClickHome = () => {
+    navigate('/');
   };
 
   return (
     <Wrapper onSubmit={handleSubmit}>
-      <input value={nickname} onChange={handler} placeholder="닉네임을 입력해 주세요." />
-      <button>방 만들기</button>
+      <InnerWrapper>
+        <input value={nickname} onChange={handleInputChange} placeholder="닉네임을 입력해 주세요." />
+        <Warning isValid={isValid}>{msg}</Warning>
+      </InnerWrapper>
+      <button disabled={!isValid}>방 만들기</button>
+      <Home onClick={handleClickHome}>처음으로</Home>
     </Wrapper>
   );
 };
@@ -24,26 +38,55 @@ export default CreateRoom;
 
 const Wrapper = styled.form`
   height: 100vh;
-  border: 1px solid green;
+  background-color: #e9eaed;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  & > input {
-    width: 25%;
-    height: 3rem;
-    padding-left: 10px;
-    font-size: 1.2rem;
-  }
+
   & > button {
     width: 10%;
-    min-width: 200px;
+    min-width: 160px;
     margin-top: 3rem;
     padding: 0.5rem 1rem;
     border: none;
+    border-radius: 5px;
     font-size: 1.2rem;
+    background-color: #d5cabd;
     &:hover {
       cursor: pointer;
     }
   }
+`;
+
+const InnerWrapper = styled.div`
+  width: 30%;
+  & > input {
+    width: 100%;
+    height: 3rem;
+    padding-left: 10px;
+    font-size: 1.2rem;
+    border: none;
+    border-radius: 5px;
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+const Warning = styled.div`
+  color: ${({ isValid }) => (isValid ? '#07bc0c' : '#e74c3c')};
+  margin-top: 3px;
+  padding-left: 10px;
+  font-size: 0.9rem;
+`;
+
+const Home = styled.div`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  &:hover {
+    cursor: pointer;
+  }
+  // make as layout component
 `;
