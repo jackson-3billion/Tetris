3c877ad7e421fe143435389aa5619864dffdd851
jc <jotace422@gmail.com>
2021-12-09 23:13:26 +0900

    [Client] feat: 메인화면에 간단한 구조를 잡기 위해 링크 추가

--- client/pages/Main.js
+++ client/pages/Main.js
@@ -1,17 +1,11 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Main = () => {
-  const getRandomId = useCallback(() => {
-    const ids = [1, 2, 3, 4, 5];
-
-    return ids[Math.floor(Math.random() * ids.length)];
-  }, []);
-
   return (
     <>
-      <div>Main 입니다</div>
-      <Link to={`/game/${getRandomId()}`}>방생성</Link>
+      <Link to="/game/create">Create Room</Link>
+      <Link to="/game/join">Join Room</Link>
     </>
   );
 };
