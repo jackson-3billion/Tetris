767814ca9f306cefac16a7e30d4a694cc38699f9
jc <jotace422@gmail.com>
2021-12-12 23:38:13 +0900

    [Client] feat: 자신과 상대의 화면이 나란히 배치되도록 구현

--- client/pages/GameRoom.js
+++ client/pages/GameRoom.js
@@ -1,61 +1,43 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import styled from '@emotion/styled';
 
-import useSocket from '@hooks/useSocket.js';
+import useSocket from '@hooks/useSocket';
+
+import Tetris from '@components/Tetris';
+import Opponent from '@components/Opponent';
 
 const GameRoom = () => {
   const { id: gameRoomId } = useParams();
-  const lastTimeRef = useRef(0);
-  const requestIdRef = useRef();
-  const socketRef = useSocket('http://localhost:9000');
+  const socketRef = useSocket(`http://localhost:9000`);
 
-  const [counter, setCounter] = useState(100);
-  const [opponentCounter, setOpponentCounter] = useState(0);
-  const [gameStarted, setGameStarted] = useState(false);
+  const [started, setStarted] = useState(false);
+  const [hasOpponent, setHasOpponent] = useState(false);
 
   useEffect(() => {
     if (socketRef?.current) {
       const socket = socketRef.current;
       socket.emit('enter', gameRoomId);
       socket.on('waiting', (data) => console.log(data));
-      socket.on('start', () => setGameStarted(true));
+      socket.on('start', () => setHasOpponent(true));
       socket.on('full', (data) => console.log(data));
       socket.on('end', () => {
-        console.log('opponent leaved the game');
-        setGameStarted(false);
+        console.log('opponent left the game');
+        setStarted(false);
       });
     }
   }, [socketRef, gameRoomId]);
 
-  useEffect(() => {
-    if (!gameStarted) {
-      cancelAnimationFrame(requestIdRef.current);
-      return;
-    }
-    const render = (now) => {
-      if (now - lastTimeRef.current > 1000) {
-        lastTimeRef.current = now;
-        setCounter((cnt) => cnt - 1);
-      }
-      requestIdRef.current = requestAnimationFrame(render);
-    };
-    render();
-  }, [gameStarted]);
-
-  useEffect(() => {
-    if (!gameStarted) return;
-    if (!socketRef?.current) return;
-    const socket = socketRef.current;
-    socket.emit('counter-change', counter);
-    socket.on('opponent-change', (data) => setOpponentCounter(data.counter));
-  }, [gameStarted, counter, socketRef]);
-
   return (
-    <>
-      <div onClick={() => setCounter((cnt) => cnt - 1)}>counter: {counter}</div>
-      <div>opponent: {opponentCounter}</div>
-    </>
+    <Wrapper>
+      <Tetris started={started} setStarted={setStarted} socketRef={socketRef} />
+      {hasOpponent && <Opponent socketRef={socketRef} />}
+    </Wrapper>
   );
 };
 
 export default GameRoom;
+
+const Wrapper = styled.div`
+  display: flex;
+`;
