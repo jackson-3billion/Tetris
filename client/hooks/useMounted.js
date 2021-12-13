2e7fa4e7f6641f7554a6fa4c4f5393c0595011e1
jc <jotace422@gmail.com>
2021-12-13 23:48:17 +0900

    [Client] feat: 컴포넌트의 mount 여부를 확인할 수 있는 custom hook 작성

---
+++ client/hooks/useMounted.js
@@ -0,0 +1,15 @@
+import { useRef, useEffect } from 'react';
+
+const useMounted = () => {
+  const mountedRef = useRef(false);
+
+  useEffect(() => {
+    mountedRef.current = true;
+
+    return () => (mountedRef.current = false);
+  }, []);
+
+  return mountedRef;
+};
+
+export default useMounted;
