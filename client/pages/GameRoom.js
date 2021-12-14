f8bd498c5aac8e72b6bdd10bb3559b6a6be96d2d
jc <jotace422@gmail.com>
2021-12-14 23:56:54 +0900

    [Client] feat: 상대 플레이어의 준비상태를 조회 후 시작할 수 있도록 로직 수정 + 여러개의 state를 useReducer를 통해 한데로 묶음

--- client/pages/GameRoom.js
+++ client/pages/GameRoom.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 
@@ -8,47 +8,78 @@ import Tetris from '@components/Tetris';
 import Opponent from '@components/Opponent';
 
 const GameRoom = () => {
+  const navigate = useNavigate();
   const { id: gameRoomId } = useParams();
   const { state: nickname } = useLocation();
-  const navigate = useNavigate();
   const socketRef = useSocket(`http://localhost:9000`);
 
-  const [joined, setJoined] = useState(false);
-  const [started, setStarted] = useState(false);
-  const [opponentNickname, setOpponentNickname] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { joined, started, isHost, isReady, opponentNickname } = state;
+
+  const onStateChange = (key) => (value) => dispatch({ payload: { [key]: value } });
+
+  console.log(state);
 
   useEffect(() => {
     if (socketRef?.current) {
       const socket = socketRef.current;
       socket.emit('join', gameRoomId);
-      socket.on('joined', () => setJoined(true));
-      socket.on('isTwoPlayer', () => {
-        socket.emit('nickname', nickname);
+      socket.on('joined', (playerNum) => {
+        dispatch({ payload: { joined: true } });
+        if (playerNum === 0) {
+          dispatch({ payload: { isHost: true } });
+        }
       });
-      socket.on('nickname', (nickname) => setOpponentNickname(nickname));
+      socket.on('isTwoPlayer', () => socket.emit('nickname', nickname));
+      socket.on('nickname', (nickname) => dispatch({ payload: { opponentNickname: nickname } }));
+      socket.on('isReady', (isReady) => dispatch({ payload: { isReady } }));
       socket.on('full', () => navigate('/game/full'));
       socket.on('end', () => {
         console.log('opponent left the game');
-        setOpponentNickname('');
-        setStarted(false);
+        dispatch({
+          payload: {
+            started: false,
+            isHost: true,
+            opponentNickname: '',
+          },
+        });
+        // setOpponentNickname('');
+        // setStarted(false);
+        // setIsHost(true);
       });
     }
   }, [socketRef, gameRoomId, nickname, navigate]);
 
   return (
     <>
-      {!joined && <div>loading</div>}
       {joined && (
         <Wrapper>
-          <Tetris started={started} setStarted={setStarted} socketRef={socketRef} />
+          <Tetris
+            started={started}
+            setStarted={onStateChange('started')}
+            isOpponentReady={isReady}
+            socketRef={socketRef}
+            isHost={isHost}
+          />
           {!!opponentNickname && <Opponent socketRef={socketRef} opponentNickname={opponentNickname} />}
         </Wrapper>
       )}
+      {!joined && <div>loading</div>}
     </>
   );
 };
 
 export default GameRoom;
+
+const initialState = {
+  joined: false,
+  started: false,
+  isHost: false,
+  isReady: false,
+  opponentNickname: '',
+};
+
+const reducer = (state, action) => ({ ...state, ...action.payload });
 
 const Wrapper = styled.div`
   display: flex;
