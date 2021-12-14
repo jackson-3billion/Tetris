2ee7ad3c009be6b9200de74d763240c2d24af8a3
jc <jotace422@gmail.com>
2021-12-14 23:55:08 +0900

    [Server] feat: 상대 플레이어의 준비상태를 확인하고 방 전체에 전파할 수 있도록 수정

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = require('express')();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
+const { isRegExp } = require('util');
 
 //const db = require('./db');
 // db.query('select * from player', (_, rows) => {
@@ -11,7 +12,7 @@ const { Server } = require('socket.io');
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3090',
+    origin: 'http://localhost:2000',
     methods: ['GET', 'POST'],
   },
 });
@@ -19,7 +20,7 @@ const PORT = 9000;
 
 io.on('connection', socket => {
   socket.on('join', gameRoomId => {
-    let playerNum = getPlayerNum(io, gameRoomId);
+    const playerNum = getPlayerNum(io, gameRoomId);
 
     if (playerNum === 2) {
       socket.emit('full', 'room is full');
@@ -27,15 +28,14 @@ io.on('connection', socket => {
     }
 
     socket.join(gameRoomId);
-    ++playerNum;
     socket.emit('joined', playerNum);
     //console.log(io.sockets.adapter.rooms.get(gameRoomId));
 
-    if (playerNum === 1) {
+    if (playerNum === 0) {
       socket.emit('waiting', 'waiting for another player to join');
     }
 
-    if (playerNum === 2) {
+    if (playerNum === 1) {
       io.to(gameRoomId).emit('isTwoPlayer', 'press start to play Tetris!');
     }
 
@@ -44,9 +44,11 @@ io.on('connection', socket => {
     });
 
     socket.on('arena-updated', arena => {
-      socket.broadcast.to(gameRoomId).emit('arena-updated', {
-        arena,
-      });
+      socket.broadcast.to(gameRoomId).emit('arena-updated', arena);
+    });
+
+    socket.on('isReady', isReady => {
+      socket.broadcast.to(gameRoomId).emit('isReady', isReady);
     });
 
     socket.on('disconnect', () => {
