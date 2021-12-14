3d8419d0bd8c1ac65175bf5ae3a49ddf274d5193
jc <jotace422@gmail.com>
2021-12-14 23:54:21 +0900

    [Client] feat: 게임 준비/시작 으로 사용되는 버튼 컴포넌트 구현

---
+++ client/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Button = ({ text, callback, color }) => {
+  return (
+    <StyledButton onClick={callback} color={color}>
+      {text}
+    </StyledButton>
+  );
+};
+
+export default Button;
+
+const StyledButton = styled.button`
+  margin: 0 0 20px 0;
+  padding: 20px;
+  min-height: 30px;
+  width: 100%;
+  border-radius: 20px;
+  border: none;
+  color: white;
+  background: ${({ color }) => color || '#333'};
+  font-size: 1rem;
+  outline: none;
+  cursor: pointer;
+`;
