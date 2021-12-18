2cf04c82614d37c892b679193eac6556253552ae
jc <jotace422@gmail.com>
2021-12-18 23:47:42 +0900

    [Client] feat: pause 버튼을 누를 경우 pause 모달이 활성화도도록 구현

--- client/pages/GameRoom.js
+++ client/pages/GameRoom.js
@@ -3,60 +3,64 @@ import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 
 import useSocket from '@hooks/useSocket';
+import useModal from '@hooks/useModal';
 
 import Tetris from '@components/Tetris';
 import Opponent from '@components/Opponent';
 import Timer from '@components/Timer';
+import Button from '@components/Button';
 
 const GameRoom = () => {
   const navigate = useNavigate();
   const { id: gameRoomId } = useParams();
   const { state: nickname } = useLocation();
   const socketRef = useSocket(`http://localhost:9000`);
+  const [isGameOverModalOpen, openGameOverModal, hideGameOverModal, GameOverModal] = useModal();
+  const [isPauseModalOpen, openPauseModal, hidePauseModal, PauseModal] = useModal();
 
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { joined, started, paused, isHost, isReady, opponentNickname } = state;
+  const { joined, started, paused, isHost, isReady, isGameOver, opponentNickname } = state;
 
-  const onStateChange = useCallback((k) => (v) => dispatch({ payload: { [k]: v } }), []);
+  const handleStateChange = useCallback((k) => (v) => dispatch({ payload: { [k]: v } }), []);
+
+  const handleResume = () => {
+    const socket = socketRef?.current;
+    socket.emit('resume');
+  };
 
   useEffect(() => {
     if (!socketRef?.current) return;
     const socket = socketRef.current;
     socket.emit('join', gameRoomId);
-    socket.on('joined', (playerNum) => {
-      dispatch({ payload: { joined: true } });
-      if (playerNum === 0) {
-        dispatch({ payload: { isHost: true } });
-      }
-    });
+
+    socket.on('joined', (playerNum) => dispatch({ payload: { joined: true, isHost: !playerNum } }));
     socket.on('isTwoPlayer', () => socket.emit('nickname', nickname));
+    socket.on('full', () => navigate('/game/full'));
     socket.on('nickname', (opponentNickname) => dispatch({ payload: { opponentNickname } }));
     socket.on('isReady', (isReady) => dispatch({ payload: { isReady } }));
-    socket.on('start', () => dispatch({ payload: { started: true } }));
-    socket.on('paused', (paused) => {
-      console.log(`paused: ${paused}`);
-      dispatch({ payload: { paused } });
-    });
-    socket.on('full', () => navigate('/game/full'));
-    socket.on('end', () => {
-      dispatch({
-        payload: {
-          started: false,
-          isHost: true,
-          opponentNickname: '',
-        },
-      });
-    });
+    socket.on('start', () => dispatch({ payload: { started: true, isGameOver: false, isWinner: false } }));
+    socket.on('paused', (paused) => dispatch({ payload: { paused } }));
+    socket.on('gameOver', (winner) => dispatch({ payload: { isGameOver: true, isWinner: nickname === winner } }));
+    socket.on('opponentLeft', () => dispatch({ payload: { started: false, isHost: true, opponentNickname: '' } }));
   }, [socketRef, gameRoomId, nickname, navigate]);
+
+  useEffect(() => {
+    paused ? openPauseModal() : hidePauseModal();
+  }, [paused, openPauseModal, hidePauseModal]);
+
+  useEffect(() => {
+    isGameOver ? openGameOverModal() : hideGameOverModal();
+  }, [isGameOver, openGameOverModal, hideGameOverModal]);
 
   return (
     <>
+      {!joined && <div>loading</div>}
       {joined && (
         <Wrapper>
           <Timer started={started} paused={paused} />
           <Tetris
             started={started}
-            setStarted={onStateChange('started')}
+            setStarted={handleStateChange('started')}
             paused={paused}
             isHost={isHost}
             isOpponentReady={isReady}
@@ -65,7 +69,13 @@ const GameRoom = () => {
           {!!opponentNickname && <Opponent socketRef={socketRef} opponentNickname={opponentNickname} />}
         </Wrapper>
       )}
-      {!joined && <div>loading</div>}
+      {isGameOverModalOpen && <GameOverModal>GAME OVER</GameOverModal>}
+      {isPauseModalOpen && (
+        <PauseModal>
+          {/* <ModalMsg>PAUSED</ModalMsg> */}
+          <Button text="RESUME" callback={handleResume} color="salmon" />
+        </PauseModal>
+      )}
     </>
   );
 };
@@ -78,6 +88,8 @@ const initialState = {
   paused: false,
   isHost: false,
   isReady: false,
+  isGameOver: false,
+  isWinner: false,
   opponentNickname: '',
 };
 
