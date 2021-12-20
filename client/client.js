cba32af4bec00d6a2fc629a76e0e58353785ecb6
jc <jotace422@gmail.com>
2021-12-20 23:48:06 +0900

    [Client] feat: context 생성 후 provider 적용

--- client/client.js
+++ client/client.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ItemsProvider } from '@contexts/items';
+
 import App from '@layouts/App';
 
 render(
   <BrowserRouter>
-    <App />
+    <ItemsProvider>
+      <App />
+    </ItemsProvider>
   </BrowserRouter>,
   document.getElementById('app'),
 );
