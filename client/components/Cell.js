b34f5c04949a35aa68605f03e4d4eb654aba1a1d
jc <jotace422@gmail.com>
2021-12-20 23:49:49 +0900

    [Client] design: 아이템 효과를 시각화하기 위해 keyframes animation 적용

--- client/components/Cell.js
+++ client/components/Cell.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styled from '@emotion/styled';
+import { keyframes, css } from '@emotion/react';
 import { lighten, darken } from 'polished';
 
 import { TETROMINOS } from '@utils/tetrominos';
 
-export const Cell = ({ type }) => {
+import { FaForward, FaBackward, FaStar, FaBomb } from 'react-icons/fa';
+
+const itemMapper = {
+  bomb: <FaBomb color="black" size="90%" />,
+  star: <FaStar color="yellow" size="90%" />,
+  faster: <FaForward color="green" size="90%" />,
+  slower: <FaBackward color="crimson" size="90%" />,
+};
+
+export const Cell = ({ type, item, exploding }) => {
   return (
     <CellBorder color={TETROMINOS[type].color}>
-      <StyledCell type={type} color={TETROMINOS[type].color} />
+      <StyledCell type={type} color={TETROMINOS[type].color} exploding={exploding}>
+        {item && itemMapper[item]}
+      </StyledCell>
     </CellBorder>
   );
 };
 
-export default Cell;
+export default memo(Cell);
+
+const sparkling = keyframes`
+ 0.00% { background-color: red;    }
+ 16.67% { background-color: orange; }
+ 33.33% { background-color: yellow; }
+ 50.00% { background-color: green;  }
+ 66.67% { background-color: blue;   }
+ 83.33% { background-color: indigo; }
+100.00% { background-color: violet; }
+`;
 
 const CellBorder = styled.div`
   border: ${({ type }) => (type === '0' ? '0px solid' : '1px solid')};
@@ -20,9 +42,21 @@ const CellBorder = styled.div`
 `;
 
 const StyledCell = styled.div`
-  //width: auto;
   height: 100%;
-  background-color: ${({ color }) => color};
+  background-color: ${({ color, exploding }) => (exploding ? 'red' : color)};
   border: ${({ type }) => (type === '0' ? 'none' : '1px solid')};
   border-color: ${({ color }) => lighten(0.3, color)};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${({ exploding }) =>
+    exploding &&
+    css`
+      animation-name: ${sparkling};
+      animation-duration: 0.5s;
+      animation-timing-function: ease-in-out;
+      animation-iteration-count: infinite;
+    `}
 `;
