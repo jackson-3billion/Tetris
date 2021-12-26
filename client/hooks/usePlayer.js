aa8a2f6c0f12e82055531f2b17f0cc80578cac3c
jc <jotace422@gmail.com>
2021-12-26 20:29:56 +0900

    [Client] refactor: player 블록을 rotate 하는 함수를 usePlayer 컴포넌트로 이동

--- client/hooks/usePlayer.js
+++ client/hooks/usePlayer.js
@@ -2,6 +2,7 @@ import { useCallback, useState } from 'react';
 
 import { TETROMINOS, randomTetromino } from '@utils/tetrominos';
 import { PLAYER_INITIAL_POS } from '@utils/constants';
+import { checkCollision, rotateMatrix, rotateItem } from '@utils/gameHelper';
 
 const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -28,7 +29,28 @@ const usePlayer = () => {
     });
   }, []);
 
-  return [player, setPlayer, movePlayer, resetPlayer];
+  const rotatePlayer = (arena) => {
+    const rotatedTetromino = rotateMatrix(player.tetromino.shape, -1);
+    player.tetromino.shape = rotatedTetromino;
+    player.tetromino.itemPos = rotateItem(player.tetromino.itemPos, rotatedTetromino.length, -1);
+    if (!checkCollision(arena, player, { x: 0, y: 0 })) {
+      return setPlayer({ ...player });
+    }
+    for (let i = 1; i <= 2; i++) {
+      if (!checkCollision(arena, player, { x: -1 * i, y: 0 })) {
+        return movePlayer({ x: -1 * i, y: 0 });
+      }
+
+      if (!checkCollision(arena, player, { x: 1 * i, y: 0 })) {
+        return movePlayer({ x: 1 * i, y: 0 });
+      }
+    }
+
+    player.tetromino.shape = rotateMatrix(player.tetromino.shape, 1);
+    player.tetromino.itemPos = rotateItem(player.tetromino.itemPos, rotatedTetromino.length, 1);
+  };
+
+  return [player, setPlayer, movePlayer, resetPlayer, rotatePlayer];
 };
 
 export default usePlayer;
