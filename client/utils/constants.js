6f061c74cfe27cb002ba8b03528dbde60910ab31
jc <jotace422@gmail.com>
2021-12-26 23:25:26 +0900

    [Client] chore: 기본 낙하 속도, 빠른 낙하 속도, 블록 좌우 이동 속도 등 상수값 변경

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,10 +1,11 @@
 export const ARENA_WIDTH = 12;
 export const ARENA_HEIGHT = 20;
 export const PLAYER_INITIAL_POS = { x: ARENA_WIDTH / 2 - 2, y: 0 };
-export const DROP_SLOW = 700;
-export const DROP_FAST = 70;
+export const DROP_SLOW = 800;
+export const DROP_FAST = 51;
+export const MIN_INTERVAL = 100;
 export const DROP_PAUSED = 99999999;
 export const LEFTWARD = { x: -1, y: 0 };
 export const RIGHTWARD = { x: 1, y: 0 };
 export const DOWNWARD = { x: 0, y: 1 };
-export const KEYHOLD_MAX_CNT = 3;
+export const KEYHOLD_MAX_CNT = 2;
