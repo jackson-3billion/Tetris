01fa2eddffaa293ee70fe985675490024e813e92
jc <jotace422@gmail.com>
2021-12-27 23:47:44 +0900

    [Client] feat: CatJam 아이템 사용시 화면에 슬라이드되는 컴포넌트 구현

---
+++ client/components/CatJam.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createPortal } from 'react-dom';
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+const CatJam = () => {
+  return createPortal(
+    <CatJamGif src="https://media2.giphy.com/media/JVglf7QjxaZZM2tjfB/giphy.gif" />,
+    document.getElementById('cat-jam'),
+  );
+};
+
+export default CatJam;
+
+const slideInAndOut = keyframes`
+    0% { transform: translateX(calc(-100%)); }
+   50% { transform: translateX(0)}
+  100% { transform: translateX(calc(-100%)); };
+`;
+
+const CatJamGif = styled.img`
+  height: 100%;
+  animation: ${slideInAndOut} 8s forwards;
+`;
