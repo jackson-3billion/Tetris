1e76cb3c84b4f389377f505c11fcce5d79e244f7
jc <jotace422@gmail.com>
2021-12-28 17:22:07 +0900

    [Client] feat: neon 폰트 추가 및 전역 스타일 추가

--- client/client.js
+++ client/client.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Global, css } from '@emotion/react';
+import neonregular2 from './fonts/neon-webfont.woff2';
+import neonregular from './fonts/neon-webfont.woff';
 
 import { StatusProvider } from '@contexts/status';
 
 import App from '@layouts/App';
 
+const styles = css`
+  @font-face {
+    font-family: 'neonregular';
+    src: url('${neonregular2}') format('woff2'), url('${neonregular}') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+
 render(
   <BrowserRouter>
     <StatusProvider>
+      <Global styles={styles} />
       <App />
     </StatusProvider>
   </BrowserRouter>,
