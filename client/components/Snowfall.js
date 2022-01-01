e2ca4d8cd6e85c31c0e56a45ead7752314647bd6
jc <jotace422@gmail.com>
2022-01-01 23:40:00 +0900

    [Client] feat: Canvas를 이용해 눈 내리는 배경 component 구현

--- client/components/Snowfall.js
+++ client/components/Snowfall.js
@@ -2,38 +2,34 @@ import React, { useRef, useEffect } from 'react';
 import styled from '@emotion/styled';
 
 import Snowflake from '@utils/snowflake';
-//import { fixDpi } from '@utils/canvas';
+import { fixDpi } from '@utils/canvas';
+import { MAX_SNOWFLAKES } from '@utils/constants';
 
 const Snowfall = () => {
   const canvasRef = useRef();
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    //fixDpi(canvas);
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
 
+  useEffect(() => {
+    let requestId;
+
+    const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    //ctx.fillStyle = ctx.strokeStyle = 'white';
-    // ctx.filter = 'blur(2px)';
+
+    fixDpi(canvas);
 
     const snowflakes = [];
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < MAX_SNOWFLAKES; i++) {
       snowflakes.push(new Snowflake(canvas, i));
     }
-    //snowflakes.forEach((snowflake) => snowflake.render());
 
     const animateFlakes = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      for (let i = 0; i < snowflakes.length; i++) {
-        snowflakes[i].update();
-      }
-      //snowflakes.forEach((snowflake) => snowflake.update());
-
-      requestAnimationFrame(animateFlakes);
+      ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+      snowflakes.forEach((snowflake) => snowflake.update());
+      requestId = requestAnimationFrame(animateFlakes);
     };
 
     animateFlakes();
+
+    return () => cancelAnimationFrame(requestId);
   }, []);
 
   return <Canvas ref={canvasRef} />;
@@ -43,6 +39,9 @@ export default Snowfall;
 
 const Canvas = styled.canvas`
   background-color: black;
+  //background-color: #0f8a5f;
   position: absolute;
   z-index: -1;
+  width: 100%;
+  height: 100%;
 `;
