42e3b3725387071274bbbb204661a6c6d6c0c101
jc <jotace422@gmail.com>
2022-01-02 18:46:27 +0900

    [Client] feat: 테트리스를 진행하면서 획득한 score를 표시하기 위해 context에 새로운 상태 및 상태변경함수 추가

--- client/contexts/status.js
+++ client/contexts/status.js
@@ -5,6 +5,7 @@ const StatusContext = createContext({
     items: [],
     level: 0,
     accel: 0,
+    score: 0,
     explodingPos: null,
     catJamming: false,
     rotated: false,
@@ -14,6 +15,7 @@ const StatusContext = createContext({
     setItems: () => {},
     setLevel: () => {},
     setAccel: () => {},
+    setScore: () => {},
     setExplodindPos: () => {},
     setCatJamming: () => {},
     setRotated: () => {},
@@ -25,13 +27,14 @@ const StatusProvider = ({ children }) => {
   const [items, setItems] = useState([]);
   const [level, setLevel] = useState(1);
   const [accel, setAccel] = useState(0);
+  const [score, setScore] = useState(0);
   const [explodingPos, setExplodingPos] = useState(null);
   const [catJamming, setCatJamming] = useState(false);
   const [rotated, setRotated] = useState(false);
   const [flipped, setFlipped] = useState(false);
   const value = {
-    state: { items, level, accel, explodingPos, catJamming, rotated, flipped },
-    actions: { setItems, setLevel, setAccel, setExplodingPos, setCatJamming, setRotated, setFlipped },
+    state: { items, level, accel, score, explodingPos, catJamming, rotated, flipped },
+    actions: { setItems, setLevel, setAccel, setScore, setExplodingPos, setCatJamming, setRotated, setFlipped },
   };
 
   return <StatusContext.Provider value={value}>{children}</StatusContext.Provider>;
