fc03cd185426a6a2769d610d6a67627f659eb9fb
jc <jotace422@gmail.com>
2022-01-02 23:24:04 +0900

    [Client] feat: 상대의 레벨을 확인할 수 있도록 level을 표시하는 Display 컴포넌트 추가

--- client/components/Opponent.js
+++ client/components/Opponent.js
@@ -12,27 +12,40 @@ import useMounted from '@hooks/useMounted';
 import { createArena } from '@utils/gameHelper';
 
 const Opponent = ({ socketRef, catJamBgmRef, opponentNickname }) => {
-  const { state } = useContext(OpponentStatusContext);
+  const {
+    state,
+    actions: { setScore, setLevel },
+  } = useContext(OpponentStatusContext);
 
   const [arena, setArena] = useState(createArena());
   const mounted = useMounted();
 
   useEffect(() => {
-    if (socketRef?.current) {
-      const socket = socketRef.current;
-      socket.on('arena-updated', (newArena) => {
-        if (!mounted) return;
-        setArena(newArena);
-      });
-    }
-  }, [socketRef, mounted]);
+    if (!socketRef?.current) return;
+
+    const socket = socketRef.current;
+    socket.on('arena-updated', (newArena) => {
+      if (!mounted) return;
+      setArena(newArena);
+    });
+    socket.on('score-updated', (score) => setScore(score));
+    socket.on('level-updated', (level) => setLevel(level));
+
+    return () => {
+      socket.off('arena-updated');
+      socket.off('score-updated');
+      socket.off('level-updated');
+    };
+  }, [socketRef, mounted, setScore, setLevel]);
 
   return (
     <TetrisWrapper>
       <TetrisGame>
         <Arena arena={arena} {...state} />
         <aside>
-          <Display text={opponentNickname ? opponentNickname : 'waiting'} />
+          <Display title="Nickname">{opponentNickname ? opponentNickname : 'waiting'}</Display>
+          <Display title="Level">{state.level}</Display>
+          <Display title="Score">{state.score}</Display>
         </aside>
       </TetrisGame>
       {state.catJamming && <CatJam catJamBgmRef={catJamBgmRef} isOpponent={true} />}
@@ -45,7 +58,6 @@ export default memo(Opponent);
 const TetrisWrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  //background: gray;
   overflow: hidden;
 `;
 
@@ -57,7 +69,6 @@ const TetrisGame = styled.div`
   max-width: 900px;
 
   aside {
-    //width: 100%;
     max-width: 200px;
     display: block;
     padding: 0 20px;
