abc949128797c7a7488417397d1ba8571204e6b7
jc <jotace422@gmail.com>
2022-01-04 01:20:23 +0900

    [Server] fix: Nodemailer와 Google 프로젝트(OAuth2)를 연동하여 less secure apps 옵션을 허용하지 않아도 메일을 발송할 수 있도록 변경

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -3,20 +3,27 @@ const nodemailer = require('nodemailer');
 
 router.post('/', (req, res) => {
   const { gameRoomId, email, inviter } = req.body;
+
   const transporter = nodemailer.createTransport({
     service: 'gmail',
+    host: 'smtp.google.com',
+    port: 587,
+    secure: true,
     auth: {
-      user: process.env.MAIL_USERNAME,
-      pass: process.env.MAIL_PASSWORD,
+      type: 'OAuth2',
+      user: process.env.OAUTH_USER,
+      clientId: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
     },
   });
 
   const mailDetails = {
-    from: 'REACT-TETRIS',
+    from: `REACT-TETRIS <${process.env.OAUTH_CLIENT_ID}>`,
     to: email,
     subject: '[Invitation] React-Tetris',
     html: `
-      <h3>${inviter}님께서 1:1 테트리스 대결을 신청하셨습니다.🎮</h3>
+      <h2>${inviter}님께서 1:1 테트리스 대결을 신청하셨습니다.🎮</h2>
       <a href="http://localhost:2000/game/join/${gameRoomId}">테트리스 게임 입장하기</a>
     `,
   };
