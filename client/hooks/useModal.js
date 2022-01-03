c1fe9ca88a3f1290eedbf84049db155b1dfa8372
jc <jotace422@gmail.com>
2022-01-03 23:45:41 +0900

    [Client] refactor: Modal을 더 확장성있게 사용할 수 있도록 구조 변경

--- client/hooks/useModal.js
+++ client/hooks/useModal.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 
 import Modal from '@components/common/Modal';
 
-const useModal = () => {
-  const [isModalOpen, setIsModalOpen] = useState(true);
+const useModal = ({ closableOverlay, styles } = {}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => setIsModalOpen(true);
   const hideModal = () => setIsModalOpen(false);
 
   const ModalContainer = ({ children }) => {
-    return <>{isModalOpen && <Modal hideModal={hideModal}>{children}</Modal>}</>;
+    return (
+      <>
+        {isModalOpen && (
+          <Modal closableOverlay={closableOverlay} hideModal={hideModal} styles={styles}>
+            {children}
+          </Modal>
+        )}
+      </>
+    );
   };
 
   return [isModalOpen, openModal, hideModal, ModalContainer];
