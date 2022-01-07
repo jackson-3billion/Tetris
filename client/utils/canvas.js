e87c91694fc8a5e335487f030263490333774cea
jc <jotace422@gmail.com>
2022-01-07 21:46:01 +0900

    [Client] chore: 불필요한 주석 제거

--- client/utils/canvas.js
+++ client/utils/canvas.js
@@ -3,10 +3,6 @@ export const fixDpi = (canvas) => {
   const style_height = +getComputedStyle(canvas).getPropertyValue('height').slice(0, -2);
   const style_width = +getComputedStyle(canvas).getPropertyValue('width').slice(0, -2);
 
-  console.log(`dpi: ${dpi}`);
-
-  console.log(`style-height: ${style_height}`);
-
   canvas.setAttribute('height', style_height * dpi);
   canvas.setAttribute('width', style_width * dpi);
 
