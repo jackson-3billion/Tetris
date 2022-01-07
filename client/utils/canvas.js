55d76ee316fda397dfb810038912bdfbd78c39cd
jc <jotace422@gmail.com>
2022-01-07 13:50:19 +0900

    [Client] fix: mobile에서 canvas 크기 조정 2차

--- client/utils/canvas.js
+++ client/utils/canvas.js
@@ -1,8 +1,15 @@
 export const fixDpi = (canvas) => {
-  //const dpi = window.devicePixelRatio;
+  const dpi = window.devicePixelRatio;
   const style_height = +getComputedStyle(canvas).getPropertyValue('height').slice(0, -2);
   const style_width = +getComputedStyle(canvas).getPropertyValue('width').slice(0, -2);
 
-  canvas.setAttribute('height', style_height);
-  canvas.setAttribute('width', style_width);
+  console.log(`dpi: ${dpi}`);
+
+  console.log(`style-height: ${style_height}`);
+
+  canvas.setAttribute('height', style_height * dpi);
+  canvas.setAttribute('width', style_width * dpi);
+
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
 };
