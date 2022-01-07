56397687a709c03c7e132b4079b4307310417298
jc <jotace422@gmail.com>
2022-01-07 13:42:59 +0900

    [Client] fix: mobile에서 canvas가 정상적인 크기로 렌더되도록 수정

--- client/utils/canvas.js
+++ client/utils/canvas.js
@@ -1,8 +1,8 @@
 export const fixDpi = (canvas) => {
-  const dpi = window.devicePixelRatio;
+  //const dpi = window.devicePixelRatio;
   const style_height = +getComputedStyle(canvas).getPropertyValue('height').slice(0, -2);
   const style_width = +getComputedStyle(canvas).getPropertyValue('width').slice(0, -2);
 
-  canvas.setAttribute('height', style_height * dpi);
-  canvas.setAttribute('width', style_width * dpi);
+  canvas.setAttribute('height', style_height);
+  canvas.setAttribute('width', style_width);
 };
