465c374322c3f718af2095b94d290919aef41b27
jc <jotace422@gmail.com>
2022-01-08 01:39:43 +0900

    [Client] chore: 구글 폰트를 삭제하고 font-face로 폰트 추가

--- client/client.js
+++ client/client.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { Global, css } from '@emotion/react';
 import neonregular2 from './fonts/neon-webfont.woff2';
 import neonregular from './fonts/neon-webfont.woff';
+import indieflower from './fonts/IndieFlower-Regular.woff';
 
 import { StatusProvider } from '@contexts/status';
 import { OpponentStatusProvider } from '@contexts/opponentStatus';
@@ -17,6 +18,13 @@ const styles = css`
   @font-face {
     font-family: 'neonregular';
     src: url('${neonregular2}') format('woff2'), url('${neonregular}') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Indie Flower';
+    src: url('${indieflower}') format('woff');
     font-weight: normal;
     font-style: normal;
   }
